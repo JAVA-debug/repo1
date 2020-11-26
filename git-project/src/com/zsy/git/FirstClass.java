4371d1623ada706dd548c90eea18bd24cd1d1225
JAVA-debug <641841158@qq.com>
2020-11-26 20:34:55 +0800

    gommit msg

---
+++ git-project/src/com/zsy/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.zsy.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        test();
+        System.out.println("hello git project");
+    }
+
+    static void test(){
+        int i = 1;
+        i += 10;
+        System.out.println(i);
+    }
+    public void adf(){
+        System.out.println("test");
+    }
+}
